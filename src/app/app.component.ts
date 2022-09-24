6d650a92a8ca28912e238a825db36d40b6e88632
DT0998 <duongkytrieuvn@gmail.com>
2022-09-25 00:16:49 +0700

    feat: finish basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,12 @@ export class AppComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
+  userName = '';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
+  showSecret = false;
+  // log: number[] = [];
+  log: Date[] = [];
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -16,10 +22,17 @@ export class AppComponent {
   }
   ngOnInit(): void {}
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = `Server was created!Name is ${this.serverName}`;
   }
   onUpdateServerName(event: Event) {
     console.log(event);
     this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  onToggleDetails() { 
+    this.showSecret = !this.showSecret; 
+    // this.log.push(this.log.length + 1);  
+    this.log.push(new Date());
   }
 }
